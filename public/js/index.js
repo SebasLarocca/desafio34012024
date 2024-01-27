b824d08bb2ef50dd84b41786dbee2a628339312a
Sebastian <sebastian_larocca@hotmail.com>
2024-01-26 23:44:23 -0300

    alineado precio de realtimeproducts

--- public/js/index.js
+++ public/js/index.js
@@ -8,7 +8,7 @@ socket.on('evento', data =>{
     let listadoProductosHTML = ''
     
     parsedData.map( element => {
-        listadoProductosHTML = listadoProductosHTML + `<tr><td>${element.title}</td><td>${element.precio}</td><td>${element.descripcion}</td><td>${element.category}</td></tr>`
+        listadoProductosHTML = listadoProductosHTML + `<tr><td>${element.title}</td><td class="align_right" >${element.precio}</td><td>${element.descripcion}</td><td>${element.category}</td></tr>`
     })
     let marcoTabla = `<table><tr><th>Producto</th><th>Precio</th><th>Descripción</th><th>Categoría</th></tr>${listadoProductosHTML}</table>`
     listadoProductos.innerHTML = marcoTabla
